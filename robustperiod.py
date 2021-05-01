46aa9ee1ff93a6f19994cef9c20bac0f533e2b58
ariaghora <hello@ghora.net>
2021-05-02 03:14:39 +0700

    Implement periodogram and graphings

--- robustperiod.py
+++ robustperiod.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from modwt import modwt
 from statsmodels.tsa.filters.hp_filter import hpfilter
 
-
 from utils import sinewave, triangle
+from mperioreg import m_perio_reg
 
 
 def extract_trend(y, reg):
@@ -29,7 +29,7 @@ def residual_autocov(x, c):
 def robust_period(x, wavelet_method, num_wavelet, lmb, c):
     '''
     Params:
-    - y: input signal with shape of (m, n), m is the number of observation and
+    - x: input signal with shape of (m, n), m is the number of observation and
          n is the number of series
     - wavelet_method: 
     - num_wavelet:
@@ -44,41 +44,85 @@ def robust_period(x, wavelet_method, num_wavelet, lmb, c):
     # ----------------
     # Extract trend and then deterend input series. Then perform residual
     # autocorrelation to remove extreme outliers.
-    trend, y_hat = extract_trend(y, lmb)
+    trend, y_hat = extract_trend(x, lmb)
     y_prime = residual_autocov(y_hat, c)
 
+    plt.plot(x, label='Input')
+    plt.plot(trend, label='Trend')
+    plt.legend()
+    plt.show()
 
     # 2) Decoupling multiple periodicities
     # ------------------------------------
     # Perform MODWT and ranking by robust wavelet variance
 
-    W = modwt(y_prime, wavelet_method, num_wavelet)
+    W = modwt(y_prime, wavelet_method, level=num_wavelet)
     # TODO wavelet variance
-
 
     # 3) Robust single periodicity detection
     # --------------------------------------
     # Compute Huber periodogram and Huber ACF
+    X = np.hstack([W, np.zeros_like(W)])
+
+    # TODO implement concurrent periodogram extraction for several series
+    periodograms = []
+    for i, x in enumerate(X):
+        print(f'Calculating periodogram for level {i+1}')
+        periodograms.append(m_perio_reg(x))
+    periodograms = np.array(periodograms)
+
+    # plt.plot(periodogram[:1000])
+    # plt.show()
+
+    return (
+        None,          # Periods
+        W,             # Wavelets
+        None,          # bivar
+        periodograms,  # P
+        None,          # pval
+        None           # ACF
+    )
 
 
-    
+def plot_robust_period(periods, W, bivar, periodograms, pval, ACF):
+    nrows = W.shape[0]
+    n_prime = periodograms.shape[1]
+    fig, axs = plt.subplots(nrows, 3, sharex=True,
+                            sharey=False, constrained_layout=True)
 
-    plt.plot(W.T[:,-1], label='y_hat')
-    # plt.plot(y_prime, label='y_prime')
-    # plt.legend()
+    for i in range(nrows):
+        axs[i, 0].plot(W[i], color='green', linewidth=1)
+        axs[i, 0].set(ylabel=f'Level {i+1}')
+        axs[i, 0].set_title('Wavelet Coef: Var=NULL', fontsize=8)
+        axs[i, 1].plot(periodograms[i][:n_prime//2], color='red', linewidth=1)
+        axs[i, 1].set_title('Periodogram: p=NULL; per_T=NULL', fontsize=8)
+        axs[i, 2].set_title(
+            'ACF: acf_T=NULL; fin_T=NULL; Period=False', fontsize=8)
+        for j in range(3):
+            axs[i, j].tick_params(axis='both', which='major', labelsize=8)
+            axs[i, j].tick_params(axis='both', which='minor', labelsize=8)
+    plt.yticks(fontsize=8)
+    plt.xticks(fontsize=8)
     plt.show()
 
 
 if __name__ == '__main__':
-    y1 = sinewave(1000, 20, 1)
-    y2 = sinewave(1000, 50, 1)
-    y3 = sinewave(1000, 100, 1)
-    tri = triangle(1000, 10)
-    noise = np.random.normal(0, 0.1, 1000)
+    m = 500
+    y1 = sinewave(m, 20, 1)
+    y2 = sinewave(m, 50, 1)
+    y3 = sinewave(m, 100, 1)
+    tri = triangle(m, 10)
+    noise = np.random.normal(0, 0.1, m)
     y = y1+y2+y3+tri+noise
-    y[500] += 10  # sudden spike
+    y[m//2] += 10  # sudden spike
 
-    lmb = 5000
+    lmb = 1000000
     c = 2
+    num_wavelets = 2
 
-    robust_period(y, 'db1', 7, lmb, c)
+    res = robust_period(y, 'db4', num_wavelets, lmb, c)
+
+    W = res[1]
+    periodograms = res[3]
+
+    plot_robust_period(None, W, None, periodograms, None, None)
