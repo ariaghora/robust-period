87e2f4ed5880b2bb8c50ea173c2f9a973f517c2c
ariaghora <hello@ghora.net>
2021-05-05 01:26:37 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='RobustPeriod',
+    version='0.1.0',
+    description=(
+        'Unofficial Implementation of RobustPeriod: Time-Frequency Mining for Robust Multiple Periodicities Detection'),
+    author='Aria Ghora Prabono',
+    author_email='hello@ghora.net',
+    url='https://github.com/ariaghora/robust-period',
+    license='MIT',
+    packages=['robustperiod'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.8'],
+)
